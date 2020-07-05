8fb2257529987b528a3df150bfc731927ffb1053
Juan Luis <kdeveloplab@gmail.com>
2020-07-05 15:30:22 -0600

    Se agrega ejemplo base de Perl

---
+++ Perl/hello_world.pl
@@ -0,0 +1,4 @@
+#!/usr/bin/env perl
+
+# This will print "Hello, World"
+print "Hello, world\n";
---
+++ Perl/hello_world_v2.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/env perl
+
+# This will print "Hello, World"
+
+print "Hello, world\n";
+
+=begin comment
+This is all part of multiline comment.
+You can use as many lines as you like
+These comment will be ignored by the
+compiler until the next = cut is encountered.
+=cut
